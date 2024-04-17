a944d2cf3463dce8f62f9682e4827599a27be256
Ahmed <01099517574med@gmail.com>
2024-04-17 18:27:32 +0200

    create custom app bar

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class NotesApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(brightness: Brightness.dark),
       debugShowCheckedModeBanner: false,
-      home:const NotesView() ,
+      home: const NotesView(),
     );
   }
 }
