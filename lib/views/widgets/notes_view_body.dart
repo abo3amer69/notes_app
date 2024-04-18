41240e32131312fc843714dd68c12a51b76164d2
Ahmed <01099517574med@gmail.com>
2024-04-18 02:22:25 +0200

    create custom note item

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:note_app/views/widgets/custom_note_item.dart';
 
 import 'custom_app_bar.dart';
 import 'custom_icon.dart';
@@ -16,6 +18,7 @@ class NotesViewBody extends StatelessWidget {
             height: 50,
           ),
           CustomAppBar(),
+          NotesItem(),
         ],
       ),
     );
