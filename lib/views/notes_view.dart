26beb158cf81e58fb3a5a24ef7eee3bc971a2129
Ahmed <01099517574med@gmail.com>
2024-04-18 15:10:41 +0200

    add show modal buttom sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/views/widgets/notes_view_body.dart';
 
+import 'widgets/add_note_buttom_sheet.dart';
+
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
@@ -8,10 +10,17 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return const AddNoteButtomSheet();
+              });
+        },
         child: const Icon(Icons.add),
       ),
-      body:const NotesViewBody(),
+      body: const NotesViewBody(),
     );
   }
 }
+
